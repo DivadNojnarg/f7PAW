1a970b4ab685df168cc928a8b9466acf2a4bf3a3
DivadNojnarg <dgranjon@ymail.com>
2019-08-25 23:33:01 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,43 @@
+var $$ = Dom7;
+
+var app = new Framework7({
+  root: '#app', // App root element
+
+  name: 'My App', // App name
+  theme: 'auto', // Automatic theme detection
+  // App root data
+  data: function () {
+    return {
+      user: {
+        firstName: 'John',
+        lastName: 'Doe',
+      },
+
+    };
+  },
+  // App root methods
+  methods: {
+    helloWorld: function () {
+      app.dialog.alert('Hello World!');
+    },
+  },
+  // App routes
+  routes: routes,
+
+  // Register service worker
+  serviceWorker: {
+    path: '/service-worker.js',
+  },
+});
+
+// Login Screen Demo
+$$('#my-login-screen .login-button').on('click', function () {
+  var username = $$('#my-login-screen [name="username"]').val();
+  var password = $$('#my-login-screen [name="password"]').val();
+
+  // Close login screen
+  app.loginScreen.close('#my-login-screen');
+
+  // Alert username and password
+  app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
+});
